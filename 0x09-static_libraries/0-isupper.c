61dc0ba74b0148595a30dccb86c99a8763170bbd
onyebuchi98 <offordaniel53@gmail.com>
2023-01-10 09:39:27 -0800

    library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character
+* @letter: the chracter to be tracked
+* Return: 1 if c is upperc
+▼
+ase, 0 otherwise
+*
+*/
+
+
+
+int _isupper(int letter)
+
+
+
+{
+
+
+
+return (letter >= 'A' && letter <= 'Z');
+
+
+
+}
